9ac86dfa0039427b652c159026c0bbb81dc87b4b
Mirko Clemente <contact@mirkoclemente.net>
2020-03-18 17:41:10 +0100

    added slope charts

--- src/pages/Trends.js
+++ src/pages/Trends.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 import { Layout } from '../layout'
 import { makeStyles } from '@material-ui/core/styles'
-import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
+
+import { useOldData } from '../hooks/useData'
 
 import Head from './components/Head'
 import Inputs from './components/Inputs'
 import DiveDeeper from './components/DiveDeeper'
 
 import Map from '../components/Map'
+import CountryPanel from '../components/CountryPanel'
+import SlopeChart from '../components/SlopeChart'
 
 const useStyles = makeStyles(theme => ({
   headLeftColumn: {
@@ -21,8 +24,13 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
+const PanelContainer = ({ children }) => (
+  <div style={{ display: 'flex', flexWrap: 'wrap' }}>{children}</div>
+)
+
 const Trends = ({ history, location }) => {
   const classes = useStyles()
+  const data = useOldData()
 
   return (
     <Layout>
@@ -36,6 +44,25 @@ const Trends = ({ history, location }) => {
       </Grid>
 
       <Map height={500} />
+
+      <PanelContainer>
+        {data.map(d => (
+          <CountryPanel
+            key={d.country}
+            data={d}
+            render={({ data, clip }) => (
+              <SlopeChart
+                data={data}
+                width={100}
+                height={300}
+                start={2015}
+                end={2031}
+                clipDomain={clip}
+              />
+            )}
+          />
+        ))}
+      </PanelContainer>
 
       <DiveDeeper
         title="Dive deeper"
