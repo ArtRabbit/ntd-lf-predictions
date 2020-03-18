c311b195ab43aa63ed40a42e35e8dc09c4f04f52
LukasV <lukas@code8.cz>
2020-03-18 16:56:08 +0100

    SImulation runs and returns result

--- src/pages/Simulator.js
+++ src/pages/Simulator.js
@@ -38,206 +38,181 @@ console.log(SimulatorEngine);
 // SimulatorEngine.documentReady();
 
 const useStyles = makeStyles(theme => ({
-  headLeftColumn: {
-    textAlign: "left"
-  },
-  headRightColumn: {
-    textAlign: "right",
-    padding: theme.spacing(4, 2)
-  },
-  tabs: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper
-  },
-  title: {
-    marginBottom: 10
-  },
-  formControl: {
-    margin: theme.spacing(0, 0, 5, 0),
-    minWidth: "100%",
-    "& > label": {}
-  },
-  contentLeftColumn: {},
-  contentRightColumn: {},
-  buttons: {
-    display: "flex",
-    flexDirection: "column",
-    "& > *": {
-      margin: theme.spacing(1)
+    headLeftColumn: {
+        textAlign: "left"
+    },
+    headRightColumn: {
+        textAlign: "right",
+        padding: theme.spacing(4, 2)
+    },
+    tabs: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper
+    },
+    title: {
+        marginBottom: 10
+    },
+    formControl: {
+        margin: theme.spacing(0, 0, 5, 0),
+        minWidth: "100%",
+        "& > label": {}
+    },
+    contentLeftColumn: {},
+    contentRightColumn: {},
+    buttons: {
+        display: "flex",
+        flexDirection: "column",
+        "& > *": {
+            margin: theme.spacing(1)
+        }
     }
-  }
 }));
 
 function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`
-  };
+    return {
+        id: `simple-tab-${index}`,
+        "aria-controls": `simple-tabpanel-${index}`
+    };
 }
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+    const { children, value, index, ...other } = props;
 
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  );
+    return (
+        <Typography component="div" role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
 }
 TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired
 };
 
 const Simulator = ({ history, location }) => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const [simParams, setSimParams] = useState({
-    ...SimulatorEngine.params,
-    covMDA2: SimulatorEngine.params.covMDA * 100
-  });
-  // tabs
+    SimulatorEngine.simControler.documentReady();
+    //   SimulatorEngine.simControler.modalConfirmation();
 
-  const [value, setValue] = React.useState(0);
-  const handleTabChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  // input
-  //   const [frequency, setFrequency] = useState(10);
-  const handleInputChange = event => {
-    // setFrequency(event.target.value);
-    setSimParams({ ...simParams, mdaFreq: event.target.value });
-  };
-
-  useEffect(() => {
-    console.log(simParams.mdaFreq);
-    console.log(simParams.covMDA);
-  }, [simParams]);
-
-  // slider
-  //  const [sliderValue, setSliderValue] = useState(50);
-  const handleSliderChange = (event, newValue) => {
-    setSimParams({
-      ...simParams,
-      covMDA: newValue / 100,
-      covMDA2: newValue
+    const [simParams, setSimParams] = useState({
+        ...SimulatorEngine.params
+        //    covMDA2: SimulatorEngine.params.covMDA * 100
     });
-    // setSliderValue(newValue);
-  };
+    // tabs
 
-  return (
-    <Layout>
-      <Grid container spacing={0}>
-        <Grid item md={8} xs={12} className={classes.headLeftColumn}>
-          <Head
-            transparent={true}
-            title="Lympahtic filariasis Prevalence Simulator"
-          />
-        </Grid>
-        <Grid item md={4} xs={12} className={classes.headRightColumn}>
-          <Typography
-            className={classes.headline}
-            color="textSecondary"
-            gutterBottom
-          >
-            Number of runs (precision) type of mosquito systemic adherence
-          </Typography>
-        </Grid>
-      </Grid>
+    const [value, setValue] = React.useState(0);
+    const handleTabChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-      <Grid container spacing={4}>
-        <Grid item md={8} xs={12} className={classes.contentLeftColumn}>
-          <Typography className={classes.title} variant="h5" component="h2">
-            Prevalence over time
-          </Typography>
+    const handleInputChange = event => {
+        // setFrequency(event.target.value);
+        setSimParams({ ...simParams, mdaFreq: event.target.value });
+    };
 
-          <div className={classes.tavs}>
-            <AppBar position="static">
-              <Tabs
-                value={value}
-                onChange={handleTabChange}
-                aria-label="simple tabs example"
-              >
-                <Tab label="Scenario 1" {...a11yProps(0)} />
-                <Tab label="Scenario 2" {...a11yProps(1)} />
-                <Tab label="Scenario 3" {...a11yProps(2)} />
-              </Tabs>
-            </AppBar>
-            <TabPanel value={value} index={0}>
-              Scenario 1
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              Scenario 2
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-              Scenario 3
-            </TabPanel>
-          </div>
-        </Grid>
-        <Grid item md={4} xs={12} className={classes.contentRightColumn}>
-          <Typography className={classes.title} variant="h5" component="h2">
-            Intervention
-          </Typography>
+    useEffect(() => {
+        console.log(simParams.mdaFreq);
+        console.log(simParams.covMDA);
+    }, [simParams]);
 
-          <FormControl className={classes.formControl}>
-            <InputLabel id="demo-simple-select-helper-label">
-              Fruequency
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-helper-label"
-              id="demo-simple-select-helper"
-              value={simParams.mdaFreq}
-              onChange={handleInputChange}
-            >
-              <MenuItem value={12}>Annual</MenuItem>
-              <MenuItem value={6}>Every 6 months</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl className={classes.formControl}>
-            <Typography id="non-linear-slider" gutterBottom>
-              Target coverage
-            </Typography>
+    const handleSliderChange = (event, newValue) => {
+        setSimParams({
+            ...simParams,
+            covMDA: newValue / 100
+        });
+        // setSliderValue(newValue);
+    };
 
-            <InputLabel id="slider"></InputLabel>
-            <Slider
-              value={simParams.covMDA2}
-              min={0}
-              step={1}
-              max={100}
-              onChange={handleSliderChange}
-              valueLabelDisplay="auto"
-              aria-labelledby="slider"
+    const [scenarioResults, setScenarioResults] = useState([]);
+    const simulatorCallback = par => {
+        //        console.log(par);
+        setScenarioResults([...scenarioResults, par]);
+    };
+    const runScenario = async () => {
+        var result = await SimulatorEngine.simControler.runScenario(simParams, simulatorCallback);
+        console.log(result);
+    };
+
+    return (
+        <Layout>
+            <Grid container spacing={0}>
+                <Grid item md={8} xs={12} className={classes.headLeftColumn}>
+                    <Head transparent={true} title="Lympahtic filariasis Prevalence Simulator" />
+                </Grid>
+                <Grid item md={4} xs={12} className={classes.headRightColumn}>
+                    <Typography className={classes.headline} color="textSecondary" gutterBottom>
+                        Number of runs (precision) type of mosquito systemic adherence
+                    </Typography>
+                </Grid>
+            </Grid>
+
+            <Grid container spacing={4}>
+                <Grid item md={8} xs={12} className={classes.contentLeftColumn}>
+                    <Typography className={classes.title} variant="h5" component="h2">
+                        Prevalence over time
+                    </Typography>
+
+                    <div className={classes.tavs}>
+                        <AppBar position="static">
+                            <Tabs value={value} onChange={handleTabChange} aria-label="simple tabs example">
+                                <Tab label="Scenario 1" {...a11yProps(0)} />
+                                <Tab label="Scenario 2" {...a11yProps(1)} />
+                                <Tab label="Scenario 3" {...a11yProps(2)} />
+                            </Tabs>
+                        </AppBar>
+                        <TabPanel value={value} index={0}>
+                            <div style={{ overflow: "hidden", height: "500px" }}>{scenarioResults[0]}</div>
+                        </TabPanel>
+                        <TabPanel value={value} index={1}>
+                            Scenario 2
+                        </TabPanel>
+                        <TabPanel value={value} index={2}>
+                            Scenario 3
+                        </TabPanel>
+                    </div>
+                </Grid>
+                <Grid item md={4} xs={12} className={classes.contentRightColumn}>
+                    <Typography className={classes.title} variant="h5" component="h2">
+                        Intervention
+                    </Typography>
+
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-helper-label">Fruequency</InputLabel>
+                        <Select labelId="demo-simple-select-helper-label" id="demo-simple-select-helper" value={simParams.mdaFreq} onChange={handleInputChange}>
+                            <MenuItem value={12}>Annual</MenuItem>
+                            <MenuItem value={6}>Every 6 months</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl className={classes.formControl}>
+                        <Typography id="non-linear-slider" gutterBottom>
+                            Target coverage
+                        </Typography>
+
+                        <InputLabel id="slider"></InputLabel>
+                        <Slider value={simParams.covMDA * 100} min={0} step={1} max={100} onChange={handleSliderChange} valueLabelDisplay="auto" aria-labelledby="slider" />
+                    </FormControl>
+
+                    <div className={classes.buttons}>
+                        <Button variant="contained" color="primary" onClick={runScenario}>
+                            UPDATE SCENARIO
+                        </Button>
+                        <Button variant="contained" color="primary">
+                            NEW SCENARIO
+                        </Button>
+                    </div>
+                </Grid>
+            </Grid>
+
+            <DiveDeeper
+                title="Get an overview"
+                links={[
+                    { to: "/hot-spots", name: "TRENDS FOR ALL COUNTRIES" },
+                    { to: "/country", name: "PROBLEM AREAS FOR ALL COUNTRIES" }
+                ]}
             />
-          </FormControl>
-
-          <div className={classes.buttons}>
-            <Button variant="contained" color="primary">
-              UPDATE SCENARIO
-            </Button>
-            <Button variant="contained" color="primary">
-              NEW SCENARIO
-            </Button>
-          </div>
-        </Grid>
-      </Grid>
-
-      <DiveDeeper
-        title="Get an overview"
-        links={[
-          { to: "/hot-spots", name: "TRENDS FOR ALL COUNTRIES" },
-          { to: "/country", name: "PROBLEM AREAS FOR ALL COUNTRIES" }
-        ]}
-      />
-    </Layout>
-  );
+        </Layout>
+    );
 };
 export default Simulator;
