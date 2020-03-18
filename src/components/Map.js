2374fa3988dfbe9dad028054cb1eae364fecc3a9
Mirko Clemente <contact@mirkoclemente.net>
2020-03-18 17:27:40 +0100

    moved map state handling to separate reducer hook, pass initialLevel props to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,113 +1,15 @@
-import React, { useReducer } from 'react'
-import ReactMapGL, {
-  Source,
-  Layer,
-  Popup,
-  WebMercatorViewport,
-  LinearInterpolator
-} from 'react-map-gl'
+import React from 'react'
+import ReactMapGL, { Source, Layer, Popup } from 'react-map-gl'
 import AutoSizer from 'react-virtualized-auto-sizer'
-import bbox from '@turf/bbox'
-import { merge } from 'lodash'
-import {
-  Slider,
-  Select,
-  MenuItem,
-  Tooltip,
-  Typography
-} from '@material-ui/core'
+import { Tooltip, Typography } from '@material-ui/core'
 import { format } from 'd3'
 import { useNewData } from '../hooks/useData'
 import useFeatureCollection from '../hooks/useFeatureCollection'
 import useFeaturesAndData from '../hooks/useFeaturesAndData'
+import useMapReducer from '../hooks/useMapReducer'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-const settings = [
-  {
-    source: 'data/country-level.csv',
-    featureSource: 'geo/africa_country_ms_0.1.json',
-    featureKey: 'ADMIN0ISO3',
-    dataKey: 'Country'
-  },
-  {
-    source: 'data/state-level.csv',
-    featureSource: 'geo/africa_state_ms_0.1.json',
-    featureKey: 'ADMIN1ID',
-    dataKey: 'StateCode'
-  },
-  {
-    source: 'data/iu-level.csv',
-    featureSource: 'geo/africa_iu_ms_0.01.json',
-    featureKey: 'IU_ID',
-    dataKey: 'IUID'
-  }
-]
-
-const initialState = {
-  year: 2020,
-  level: 0,
-  Regime: 'No MDA',
-  Endemicity: 0,
-  viewport: {
-    latitude: 0,
-    longitude: 20,
-    zoom: 2.7
-  }
-}
-
-const reducer = (state, { type, payload }) => {
-  switch (type) {
-    case 'LEVEL':
-      return { ...state, level: payload, feature: null, ...settings[payload] }
-    case 'REGIME':
-      return { ...state, Regime: payload }
-    case 'ENDEMICITY':
-      return { ...state, Endemicity: payload }
-    case 'YEAR':
-      return { ...state, year: payload }
-    case 'VIEWPORT':
-      return { ...state, viewport: payload }
-    case 'HOVER':
-      return {
-        ...state,
-        featureHover: payload.feature,
-        tooltip: payload.event.point
-      }
-    case 'SELECT':
-      const { feature, event } = payload
-      const [minLng, minLat, maxLng, maxLat] = bbox(feature)
-      const viewport = new WebMercatorViewport(state.viewport)
-      const { longitude, latitude, zoom } = viewport.fitBounds(
-        [
-          [minLng, minLat],
-          [maxLng, maxLat]
-        ],
-        {
-          padding: 40
-        }
-      )
-
-      return merge({}, state, {
-        feature,
-        popup: event.lngLat,
-        viewport: {
-          longitude,
-          latitude,
-          zoom,
-          transitionInterpolator: new LinearInterpolator({
-            around: [event.offsetCenter.x, event.offsetCenter.y]
-          }),
-          transitionDuration: 1000
-        }
-      })
-    case 'DESELECT':
-      return { ...state, feature: null }
-    default:
-      return state
-  }
-}
-
-function Map({ width, height }) {
+function Map({ width, height, initialLevel }) {
   const [
     {
       year,
@@ -122,10 +24,10 @@ function Map({ width, height }) {
       feature,
       featureHover,
       popup,
-      tooltip
+      tooltip,
     },
-    dispatch
-  ] = useReducer(reducer, merge({}, initialState, settings[0]))
+    dispatch,
+  ] = useMapReducer({ initialLevel })
 
   // load geojson features
   const { featureCollection, loading: loadingFeatures } = useFeatureCollection(
@@ -137,16 +39,16 @@ function Map({ width, height }) {
     Regime,
     Endemicity: level === 2 && Endemicity,
     source,
-    key: dataKey
+    key: dataKey,
   })
 
   // merge features with data
-  const { merged, ticks } = useFeaturesAndData({
+  const { merged } = useFeaturesAndData({
     featureCollection,
     data,
     stats,
     key: featureKey,
-    ready: !loading && !loadingFeatures
+    ready: !loading && !loadingFeatures,
   })
 
   const prevalenceHover =
@@ -155,22 +57,6 @@ function Map({ width, height }) {
   const selectedFeatureID = feature?.properties.id
   const selectedData = data[selectedFeatureID]
   const prevalenceSelected = selectedData?.prevalence[`Prev_Year${year}`]
-
-  const handleLevelChange = event => {
-    dispatch({ type: 'LEVEL', payload: event.target.value })
-  }
-
-  const handleRegimeChange = event => {
-    dispatch({ type: 'REGIME', payload: event.target.value })
-  }
-
-  const handleEndemicityChange = event => {
-    dispatch({ type: 'ENDEMICITY', payload: event.target.value })
-  }
-
-  const handleYearChange = (event, payload) => {
-    dispatch({ type: 'YEAR', payload })
-  }
 
   const handleViewportChange = payload => {
     dispatch({ type: 'VIEWPORT', payload })
@@ -197,37 +83,37 @@ function Map({ width, height }) {
       {...viewport}
       width={width}
       height={height}
-      mapStyle='mapbox://styles/kpcarter100/ck7w5zz9l026d1imn43721owm'
+      mapStyle="mapbox://styles/kpcarter100/ck7w5zz9l026d1imn43721owm"
       interactiveLayerIds={['fill-layer']}
       onViewportChange={handleViewportChange}
       onClick={handleClick}
       onHover={handleHover}
     >
-      <Source id='africa' type='geojson' data={merged}>
+      <Source id="africa" type="geojson" data={merged}>
         <Layer
-          id='fill-layer'
+          id="fill-layer"
           // beforeId={level === 0 ? 'admin-1-boundary-bg' : 'admin-0-boundary'}
-          beforeId='admin-0-boundary'
+          beforeId="admin-0-boundary"
           filter={['has', `Prev_Year${year}`]}
-          type='fill'
+          type="fill"
           paint={{
             'fill-color': [
               'coalesce',
               ['get', `Prev_Year${year}`],
               // hide shape if no data available
-              'rgba(0,0,0,0)'
+              'rgba(0,0,0,0)',
             ],
             'fill-outline-color': [
               'case',
               ['==', ['get', 'id'], feature?.properties.id || null],
               'rgba(121, 145, 170, 1)',
-              'rgba(121, 145, 170, 0.3)'
-            ]
+              'rgba(121, 145, 170, 0.3)',
+            ],
           }}
         />
         <Layer
-          id='hover-layer'
-          type='line'
+          id="hover-layer"
+          type="line"
           filter={['has', `Prev_Year${year}`]}
           layout={{ 'line-join': 'bevel' }}
           paint={{
@@ -235,9 +121,9 @@ function Map({ width, height }) {
               'case',
               ['==', ['get', 'id'], featureHover?.properties.id || null],
               '#96B4D3',
-              'rgba(0,0,0,0)'
+              'rgba(0,0,0,0)',
             ],
-            'line-width': 2
+            'line-width': 2,
           }}
         />
       </Source>
@@ -248,10 +134,10 @@ function Map({ width, height }) {
           closeButton={true}
           closeOnClick={false}
           onClose={handleClose}
-          anchor='top'
+          anchor="top"
         >
           <div>
-            <Typography variant='subtitle1'>
+            <Typography variant="subtitle1">
               {feature.properties.name}
             </Typography>
             <div>Prevalence: {prevalenceSelected} %</div>
@@ -264,13 +150,13 @@ function Map({ width, height }) {
         <Tooltip
           title={`${featureHover.properties.name} ${prevalenceHover} %`}
           open
-          placement='top'
+          placement="top"
         >
           <span
             style={{
               position: 'absoulte',
               display: 'inline-block',
-              transform: `translate(${tooltip[0]}px,${tooltip[1] - 16}px)`
+              transform: `translate(${tooltip[0]}px,${tooltip[1] - 16}px)`,
             }}
           ></span>
         </Tooltip>
@@ -279,8 +165,8 @@ function Map({ width, height }) {
   )
 }
 
-export default ({ height }) => (
+export default ({ height, ...props }) => (
   <AutoSizer disableHeight>
-    {({ width }) => <Map width={width} height={height}></Map>}
+    {({ width }) => <Map width={width} height={height} {...props}></Map>}
   </AutoSizer>
 )
--- src/pages/HotSpots.js
+++ src/pages/HotSpots.js
@@ -1,51 +1,49 @@
-import React from 'react';
+import React from 'react'
 
-import { Layout } from '../layout';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
+import { Layout } from '../layout'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
 
-import Head from './components/Head';
-import Inputs from './components/Inputs';
-import DiveDeeper from './components/DiveDeeper';
+import Head from './components/Head'
+import Inputs from './components/Inputs'
+import DiveDeeper from './components/DiveDeeper'
+
+import Map from '../components/Map'
 
 const useStyles = makeStyles(theme => ({
-    headLeftColumn: {
-        textAlign: 'left'
-    },
-    headRightColumn: {
-        textAlign: 'right',
-        padding: theme.spacing(2),
-    },
-}));
+  headLeftColumn: {
+    textAlign: 'left',
+  },
+  headRightColumn: {
+    textAlign: 'right',
+    padding: theme.spacing(2),
+  },
+}))
 
 const HotSpots = ({ history, location }) => {
+  const classes = useStyles()
 
-    const classes = useStyles();
+  return (
+    <Layout>
+      <Grid container spacing={0}>
+        <Grid item md={5} xs={12} className={classes.headLeftColumn}>
+          <Head transparent={true} title="Lympahtic filariasis Problem areas" />
+        </Grid>
+        <Grid item md={7} xs={12} className={classes.headRightColumn}>
+          <Inputs />
+        </Grid>
+      </Grid>
 
+      <Map height={500} initialLevel={1} />
 
-    return (
-        <Layout>
-
-
-            <Grid container spacing={0}>
-                <Grid item md={5} xs={12} className={classes.headLeftColumn}>
-                    <Head
-                        transparent={true}
-                        title="Lympahtic filariasis Problem areas"
-                    />
-                </Grid>
-                <Grid item md={7} xs={12} className={classes.headRightColumn}>
-                    <Inputs />
-                </Grid>
-            </Grid>
-
-            <DiveDeeper
-                title="Dive deeper"
-                links={[{ to: '/trends', name: 'Trends' }, { to: '/country', name: 'SELECT COUNTRY' }]}
-            />
-
-        </Layout >
-    )
+      <DiveDeeper
+        title="Dive deeper"
+        links={[
+          { to: '/trends', name: 'Trends' },
+          { to: '/country', name: 'SELECT COUNTRY' },
+        ]}
+      />
+    </Layout>
+  )
 }
-export default HotSpots;
+export default HotSpots
